4f8e225e0ba5794501a77c0aefd7212c07a26558
cricht-bc <c.richter@bellevuecollege.edu>
2024-02-04 20:04:48 -0800

    habitlist additions

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+
 let habitArray = [];
  
-let Habit = function (name, category, day, time) {
+let Habit = function (name, category, day, time) { //habbit definitions instantiation
     this.name = name;
     this.category = category;
     this.day = day;
@@ -14,7 +15,7 @@ let selectedCategory = "";
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    habitList();
+        habitList();
 
     document.getElementById("addButton").addEventListener("click", function(){
     
@@ -25,9 +26,23 @@ document.addEventListener("DOMContentLoaded", function (event) {
     })
 
     document.getElementById("category").addEventListener("change", function () {
-        selectedCategory = document.getElementById("category").value;
+       selectedCategory = document.getElementById("category").value;
     });
 
 });
+
+    // function defintions
+function habitList() {
+    // clear prior data
+    var oneUL = document.getElementById("oneUL");
+    oneUL.innerHTML = "";
+
+    NoteArray.forEach(function (habitArray,) {   // use handy array forEach method
+           var li = document.createElement('li');
+           // added data-role="listview" to the ul in the html
+           li.innerHTML = habbitArray.name + "  " + habitArray.category + "   " + habbitArray.day + "   " + habbitArray.time;
+           oneUL.appendChild(li);
+    });
+}; 
 
 
